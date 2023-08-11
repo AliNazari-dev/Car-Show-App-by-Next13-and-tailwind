85838b48461a218cfd99648c6cd813be849a1d7e
Ali <AliNazarii7394@gmail.com>
2023-08-11 23:42:50 +0330

    fetchCar completet by adding searching params

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "33e7f8a5e6msh2b5af455fa1fa3fp11a7d9jsn82ba1def509b",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3", {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers,
   });
   // Parse the response as JSON
